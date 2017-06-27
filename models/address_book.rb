ea96c840aaa66bc560715eb5a7f27a6ade3dff1a
Catherine Stoll <KateMstoll@gmail.com>
2017-06-26 22:31:19 -0700

    Checkpoint 6 Reading CVS

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,21 +1,30 @@
-require_relative 'entry'
+require_relative "entry.rb"
+require "csv"
 
 class AddressBook
-  attr_reader :entries
+  attr_accessor :entries
 
   def initialize
-     @entries = []
-   end
+    @entries = []
+  end
 
-  def add_entry(name, phone_number, email)
+  def add_entry(name, phone, email)
     index = 0
-     entries.each do |entry|
-       if name < entry.name
-         break
-       end
-       index+= 1
-     end
+    @entries.each do |entry|
+      if name < entry.name
+        break
+      end
+      index += 1
+    end
+    @entries.insert(index, Entry.new(name, phone, email))
+  end
 
-     entries.insert(index, Entry.new(name,phone_number,email))
-   end
- end
+  def import_from_csv(file_name)
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true)
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+    end
+  end
+end
